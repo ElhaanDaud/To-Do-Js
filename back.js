6114ce9a55f957a672bbd8ab40d088b541657645
ElhaanDaud <elhaan.daud786@gmail.com>
2024-05-21 00:38:35 +0530

    Js file

--- back.js
+++ back.js
@@ -1,10 +1,27 @@
 
 const container= document.querySelector('.container')
+const todoListBox= document.querySelector('.todoListBox')
+const ul_li=document.querySelector('ul li')
+
+
 const images = [
     "url('images/bg1.jpg')",
     "url('images/bg2.webp')",
     "url('images/bg3.jpg')"
 ];
+
+const todoColor = [
+    "rgba(103, 50, 23,0.5)",
+   "rgba(41, 74, 42,0.5)",
+    "rgba(27, 102, 187,0.5)"
+];
+
+const taskColor=[
+    "rgba(226, 152, 117, 0.7)",
+    "rgba(11, 46, 94, 0.7)",
+    " rgba(41, 74, 42, 0.7)"
+]
+
 
 const inputBox = document.getElementById('input-box');
 
@@ -12,6 +29,26 @@ const listContainer = document.getElementById('list-container');
 
 
 container.style.backgroundImage=images[0]
+
+
+function redImage(){
+    container.style.backgroundImage=images[0];
+    todoListBox.style.backgroundColor=todoColor[0];
+    ul_li.style.backgroundColor=taskColor[0];
+}
+
+function greenImage(){
+    container.style.backgroundImage=images[1];
+    todoListBox.style.backgroundColor=todoColor[1];
+    ul_li.style.backgroundColor=taskColor[1];
+
+}
+function blueImage(){
+    container.style.backgroundImage=images[2];
+    todoListBox.style.backgroundColor=todoColor[2];
+    ul_li.style.backgroundColor=taskColor[2];
+}
+
 
 /*function cycleImages(index) {
     let count = 0;
